30329d9dbb70120b0f1863aca7136d36547c9823
ceailo <s46390@student.umt.edu.my>
2023-02-05 03:35:05 +0800

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,15 @@ import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import studio from "@theatre/studio";
+import { getProject } from "@theatre/core";
+import extension from "@theatre/r3f/dist/extension";
+import { editable as e, SheetProvider } from "@theatre/r3f";
+
+// Initialize the studio
+studio.initialize();
+studio.extend(extension);
+const chukSheet = getProject("chukSheet").sheet("chuckSheet");
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -20,12 +29,15 @@ function App() {
         height: "100vh",
       }}
     >
-      <OrbitControls />
-      <pointLight color="yellow" intensity={2} position={[-1, 1, 4]} />
-      <mesh position={[1, 1, 3]}>
-        <boxGeometry args={[1, 1, 4]} />
-        <meshStandardMaterial color="#FFED00" />
-      </mesh>
+      {/* Snapshot visibility */}
+      <SheetProvider sheet={chukSheet}>
+        <OrbitControls />
+        <e.pointLight theatreKey="Light 1" color="yellow" intensity={2} position={[-1, 1, 4]} />
+        <mesh position={[1, 1, 3]}>
+          <boxGeometry args={[1, 1, 4]} />
+          <meshStandardMaterial color="#FFED00" />
+        </mesh>
+      </SheetProvider>
     </Canvas>
   );
 }
